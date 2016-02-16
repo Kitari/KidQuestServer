f887552930eae3e9c4c9944a30678f22affc1f50
Michael Porter <m_porter@live.co.uk>
2016-02-16 16:10:50 +0000

    Made server more flexible between different dev environments

---
+++ linux-dev-settings.py
@@ -0,0 +1,6 @@
+# config
+DATABASE = '/home/michael/PycharmProjects/KidQuestServer/kidquest.db'
+DEBUG = True
+SECRET_KEY = 'DEVKEY'
+USERNAME = 'admin'
+PASSWORD = 'default'
---
+++ windows-dev-settings.py
@@ -0,0 +1,6 @@
+# config
+DATABASE = 'C:\\Users\\m_por\\Databases\\KidQuest.db'
+DEBUG = True
+SECRET_KEY = 'DEVKEY'
+USERNAME = 'admin'
+PASSWORD = 'default'
