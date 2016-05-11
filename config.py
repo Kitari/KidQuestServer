ae2b4299e985111efc05590a18699f6f0cbed30f
Mike Porter <Mike Porter>
2016-05-11 19:09:50 +0100

    Updated secret key

--- config.py
+++ config.py
@@ -6,6 +6,6 @@ SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.sqlite')
 SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
 DEBUG = True
 SQLALCHEMY_TRACK_MODIFICATIONS = False
-SECRET_KEY = "HELLOFRIENDS2"
+SECRET_KEY = "Nw0IhH326OTYNCRZx04d6Q263c7mnNgJ"
 
 GCM_API_KEY = "AIzaSyBO6iNSbCZtRWJeFBu1oHOJLtJQS_0YTu8"
